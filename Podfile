96eb88859fd1c41679454ec5617735381367118e
Noel-96 <noelorion69@gmail.com>
2019-02-21 10:53:04 +0100

    functional SWReveal

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '9.0'
+
+target 'Slack-Clone' do
+     use_frameworks!
+
+#pods foe slack clone
+
+pod 'Alamofire', '4.5.0'
+pod 'SwiftyJSON', '3.1.4'
+
+
+end
---
+++ Slack-Clone/controller/ChannelVC.swift
@@ -0,0 +1,24 @@
+ //
+//  ChannelVC.swift
+//  Slack-Clone
+//
+//  Created by Noel Obaseki on 21/02/2019.
+//  Copyright © 2019 Noel Obaseki. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
