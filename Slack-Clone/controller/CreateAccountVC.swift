dd0c8f1f208fa6a00c753d937c5e809213a77dd1
Noel-96 <noelorion69@gmail.com>
2019-02-22 16:57:11 +0100

    Register User Functionality is done

--- Slack-Clone/controller/CreateAccountVC.swift
+++ Slack-Clone/controller/CreateAccountVC.swift
@@ -10,17 +10,45 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
- 
+    
+    
+    @IBOutlet weak var usernameTxt: CustomSignInTextField!
+    
+    @IBOutlet weak var emailTxt: CustomSignInTextField!
+    
+    
+    @IBOutlet weak var passwordTxt: CustomSignInTextField!
+    
+    @IBOutlet weak var userImg: UIImageView!
     
     
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-
-
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTxt.text ,emailTxt.text != "" else{return}
+        
+        guard let password = passwordTxt.text, passwordTxt.text != "" else{return}
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("registered user!")
+            }
+        }
+    }
+    
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func pickBgColorPressed(_ sender: Any) {
+    }
+    
 }
