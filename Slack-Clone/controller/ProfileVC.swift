c38dc1ab78c0f5f7877b4c1bb83a2509ee79a86e
Noel-96 <noelorion69@gmail.com>
2019-03-18 14:57:45 +0100

    profile view dialog designed

---
+++ Slack-Clone/controller/ProfileVC.swift
@@ -0,0 +1,54 @@
+//
+//  ProfileVC.swift
+//  Slack-Clone
+//
+//  Created by Noel Obaseki on 18/03/2019.
+//  Copyright © 2019 Noel Obaseki. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    
+    @IBOutlet weak var userName: UILabel!
+    
+    @IBOutlet weak var userEmail: UILabel!
+    
+    
+    @IBOutlet weak var bgView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func logoutPressed(_ sender: Any) {
+        UserDataService.instance.logoutUser()
+        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    func setupView() {
+        userName.text = UserDataService.instance.name
+        userEmail.text = UserDataService.instance.email
+        profileImg.image = UIImage(named: UserDataService.instance.avatarName)
+        profileImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(ProfileVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+}
--- Slack-Clone/utils/Constants.swift
+++ Slack-Clone/utils/Constants.swift
@@ -9,37 +9,42 @@
 import Foundation
 
  
- //COMPLETION HANDLER 
  typealias CompletionHandler = (_ Success: Bool) -> ()
-
-//segues
- let TO_LOGIN = "toLogin"
- let TO_CREATE_ACCOUNT = "toCreateAccount"
- let UNWIND = "unwindtochannel"
  
- let TO_AVATAR_PICKER = "toAvatarPicker"
- 
- //User Defaults
- let TOKEN_KEY     = "token"
- let LOGGED_IN_KEY = "loggedin"
- let USER_EMAIL    = "userEmail"
+ // URL Constants
+ let BASE_URL = "https://chattychatjb.herokuapp.com/v1/"
+ let URL_REGISTER = "\(BASE_URL)account/register"
+ let URL_LOGIN = "\(BASE_URL)account/login"
+ let URL_USER_ADD = "\(BASE_URL)user/add"
+ let URL_USER_BY_EMAIL = "\(BASE_URL)user/byEmail/"
  
  // Colors
  let smackPurplePlaceholder = #colorLiteral(red: 0.3254901961, green: 0.4215201139, blue: 0.7752227187, alpha: 0.5)
  
  // Notification Constants
  let NOTIF_USER_DATA_DID_CHANGE = Notification.Name("notifUserDataChanged")
-
  
- //Base_Url_constant
+ // Segues
+ let TO_LOGIN = "toLogin"
+ let TO_CREATE_ACCOUNT = "toCreateAccount"
+ let UNWIND = "unwindToChannel"
+ let TO_AVATAR_PICKER = "toAvatarPicker"
  
- let  BASE_URL =  "https://slack--clone.herokuapp.com/v1/"
- let  URL_REGISTER = "\(BASE_URL)account/register "
- let URL_LOGIN = "\(BASE_URL)account/login"
- let URL_USER_ADD = "\(BASE_URL)user/add"
- 
+ // User Defaults
+ let TOKEN_KEY = "token"
+ let LOGGED_IN_KEY = "loggedIn"
+ let USER_EMAIL = "userEmail"
  
  // Headers
  let HEADER = [
     "Content-Type": "application/json; charset=utf-8"
  ]
+ 
+ let BEARER_HEADER = [
+    "Authorization":"Bearer \(AuthService.instance.authToken)",
+    "Content-Type": "application/json; charset=utf-8"
+ ]
+ 
+ 
+ 
+ 
